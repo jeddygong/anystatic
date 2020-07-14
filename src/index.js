e1beb3a614bfe83cf14a12f0cf6386482c54b298
gongjin <gognjin2012@gmail.com>
2020-07-14 22:20:49 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Description:
+ * @Author: Jeddy
+ * @Date: 2020-07-14 22:14:59
+ * @LastEditors: Jino
+ * @LastEditTime: 2020-07-14 22:17:23
+ */
+
+console.log("init");
+
+function test (){
+	alert(222);
+}
